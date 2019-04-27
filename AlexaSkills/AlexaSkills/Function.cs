8bf4e8a40ff306130367c0d5ba7461e9a31c4cc1
Dorothy Lam <DHLam@ou.edu>
2019-04-28 01:29:24 +0300

    oh my

--- AlexaSkills/AlexaSkills/Function.cs
+++ AlexaSkills/AlexaSkills/Function.cs
@@ -1,44 +1,72 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Threading.Tasks;
-
+using System.Net.Http;
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response;
 using Amazon.Lambda.Core;
-using Amazon.Lambda.APIGatewayEvents;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
-namespace AlexaSkills
+namespace AlexaWeather
 {
-    public class Functions
+    public class Function
     {
-        /// <summary>
-        /// Default constructor that Lambda will invoke.
-        /// </summary>
-        public Functions()
+        public const string innvocation = "Weather";
+
+        public Function()
         {
+            _httpClient = new HttpClient();
+        }
+        /// <summary>
+        /// A simple function that takes a string and does a ToUpper
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="context"></param>
+        private static HttpClient httpClient;
+        private HttpClient _httpClient;
+
+        public SkillResponse FunctionHandler(SkillRequest input, ILambdaContext context)
+        {
+            var requestType = input.GetRequestType();
+            if (requestType == typeof(IntentRequest))
+            {
+                return WeatherResponse(
+                    $"Hello, I will check the weather. Give me one moment.", true);
+            }
+            else
+            {
+                return WeatherResponse(
+                    $"I'm not sure what you're asking me. Please say something like Alexa, ask {innvocation} about the forecast in Norman, Oklahoma",
+                    true);
+            }
+
         }
 
-
-        /// <summary>
-        /// A Lambda function to respond to HTTP Get methods from API Gateway
-        /// </summary>
-        /// <param name="request"></param>
-        /// <returns>The list of blogs</returns>
-        public APIGatewayProxyResponse Get(APIGatewayProxyRequest request, ILambdaContext context)
+        private SkillResponse WeatherResponse(string outputSpeech,
+              bool endSession,
+              string repromptText = "Just say, tell me a little more about this week's weather. To exit, say, thank you.")
         {
-            context.Logger.LogLine("Get Request\n");
-
-            var response = new APIGatewayProxyResponse
+            var response = new ResponseBody
             {
-                StatusCode = (int)HttpStatusCode.OK,
-                Body = "Hello AWS Serverless",
-                Headers = new Dictionary<string, string> { { "Content-Type", "text/plain" } }
+                ShouldEndSession = endSession,
+                OutputSpeech = new PlainTextOutputSpeech { Text = outputSpeech }
+
             };
 
-            return response;
+            if (repromptText != null)
+            {
+                response.Reprompt = new Reprompt() { OutputSpeech = new PlainTextOutputSpeech() { Text = repromptText } };
+            }
+
+            var alexaResponse = new SkillResponse
+            {
+                Response = response,
+                Version = "1.0"
+            };
+
+            return alexaResponse;
         }
+
+
     }
 }
