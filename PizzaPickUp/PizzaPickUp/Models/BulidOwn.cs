6628b69321555113ead88d2a3157265d4bb67f37
Kathleen Wang <kathleen@ou.edu>
2019-05-01 12:43:10 -0500

    Create own pizaa

---
+++ PizzaPickUp/PizzaPickUp/Models/BulidOwn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PizzaPickUp.Models
+{
+    public class BulidOwn
+    {
+        public string Size { get; set; }
+        public string Topping { get; set; }
+        public string CrustOption { get; set; }
+        public string Sauce { get; set; }
+
+    }
+}
