76f1eb0e947a90de16b1ddb55313edd97d130033
Kaitlyn Freeborn <kaitlynfreeborn@ou.edu>
2019-05-01 12:31:02 -0500

    menu

---
+++ PizzaPickUp/PizzaPickUp/Controllers/MenuController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PizzaPickUp.Models;
+
+namespace PizzaPickUp.Controllers
+{
+    public class MenuController : Controller
+    {
+        // GET: Menu
+        public ActionResult Index()
+        {
+            Menu cheesePizza = new Menu()
+            {
+                itemName = "Cheese Pizza",
+                itemPrice = "8.75",
+                description = "A cheesy pizza made with 5 different cheeses",
+                itemImg = @"http://www.hammockpizza.co/image/cache/pizza/cheese-pizza-800x800.png"
+            };
+
+            List<Menu> mn = new List<Menu>();
+            mn.Add(cheesePizza);
+
+            return View(mn);
+        }
+    }
+}
---
+++ PizzaPickUp/PizzaPickUp/Models/Menu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PizzaPickUp.Models
+{
+    public class Menu
+    {
+        public string itemName { get; set; }
+        public string itemImg { get; set; }
+        public string itemPrice { get; set; }
+        public string description { get; set; }
+
+    }
+}
